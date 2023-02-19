655575c08cdf7f16a0b03d561ab93017efb14499
Reema <reematechcampus@gmail.com>
2023-02-19 10:50:06 +0300

    ffinal edits

--- contact/tests.py
+++ contact/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+
+from django.test import TestCase ,SimpleTestCase
+from django.urls import  reverse,resolve
+
+from .views import show_contact
 
 # Create your tests here.
+class ViewsTestCase(TestCase):
+    def test_index_loads_properly(self):
+        """The index page loads properly"""
+        response = self.client.get('http://127.0.0.1:8000')
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_exists(self):
+        response = self.client.get("/contact")
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('contact'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'contact/faculty_member.html')
--- learn/tests.py
+++ learn/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
+from django.urls import  reverse,resolve
+
+
+
+# Create your tests here.
+class ViewsTestCase(TestCase):
+
+
+    def test_url_exists(self):
+        response = self.client.get("/blog")
+        self.assertEqual(response.status_code, 404)
+
 # Create your tests here.
